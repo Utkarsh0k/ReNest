ca1dfdbed9c86415423fa5921d2e2e9e3309fe2b
Utkarsh Yadav <utkarshnk123@gmail.com>
2026-08-08 00:50:17 +0530

    Fixed Server Crash Due To Delete Operation in MArketplace

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -124,3 +124,42 @@ exports.deleteProduct = async (req, res) => {
     }
 
 };
+exports.markSold = async (req, res) => {
+
+    try {
+
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+
+            return res.status(404).json({
+                message: "Product not found"
+            });
+
+        }
+
+        if (product.seller.toString() !== req.user.id) {
+
+            return res.status(403).json({
+                message: "Not authorized"
+            });
+
+        }
+
+        product.status = "Sold";
+
+        await product.save();
+
+        res.json(product);
+
+    }
+
+    catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -5,16 +5,21 @@ const router = express.Router();
 const protect = require("../middleware/authMiddleware");
 
 const {
-
-    createProduct,
-
     getProducts,
-
-    getProduct
-
+    getProduct,
+    createProduct,
+    deleteProduct,
+    markSold
 } = require("../controllers/productController");
+
 router.get("/", getProducts);
+
 router.get("/:id", getProduct);
+
 router.post("/", protect, createProduct);
+
+router.delete("/:id", protect, deleteProduct);
+
+router.put("/:id/sold", protect, markSold);
 
 module.exports = router;
